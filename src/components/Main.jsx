c089a38916ceecd1daed89de26c9eb7840994775
niiquash <qua16006@byui.edu>
2025-01-15 11:44:03 -0700

    completed main component: inputs, button, image, text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,40 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div>
-      <h2>Main Component</h2>
-    </div>
+    <main className="main">
+      <div className="main__form">
+        <label>
+          Top text
+          <input
+            type="text"
+            name="top-text"
+            id="top-text"
+            placeholder="One cannot simply"
+          />
+        </label>
+        <label>
+          Bottom text
+          <input
+            type="text"
+            name="bottom-text"
+            id="bottom-text"
+            placeholder="Walk into Mordor"
+          />
+        </label>
+      </div>
+
+      <button>Get a new meme image</button>
+
+      <div className="main__image--container">
+        <img
+          src="http://i.imgflip.com/1bij.jpg"
+          alt="meme image"
+          className="main__image"
+        />
+        <span className="top">One does not simply</span>
+        <span className="bottom">Walk into Mordor</span>
+      </div>
+    </main>
   );
 };
 
