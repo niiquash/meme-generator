d2c2c9a66f359fec8190164fb2ddd655c96ce314
niiquash <qua16006@byui.edu>
2025-01-15 17:21:47 -0700

    completed fetching memes from url. refactored code to dynamically change image.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Main = () => {
+  const [meme, setMeme] = useState({
+    topText: "one does not simply",
+    bottomText: "walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  const getImage = () => {
+    const randomIndex = Math.floor(Math.random() * allMemes.length);
+    const newMeme = allMemes[randomIndex].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      imageUrl: newMeme,
+    }));
+  };
+
+  const handleChange = (event) => {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  };
+
   return (
     <main className="main">
       <div className="main__form">
@@ -8,32 +39,30 @@ const Main = () => {
           Top text
           <input
             type="text"
-            name="top-text"
-            id="top-text"
-            placeholder="One cannot simply"
+            name="topText"
+            placeholder="one does not simply"
+            onChange={handleChange}
+            value={meme.topText}
           />
         </label>
         <label>
           Bottom text
           <input
             type="text"
-            name="bottom-text"
-            id="bottom-text"
-            placeholder="Walk into Mordor"
+            name="bottomText"
+            placeholder="walk into Mordor"
+            onChange={handleChange}
+            value={meme.bottomText}
           />
         </label>
       </div>
 
-      <button>Get a new meme image</button>
+      <button onClick={getImage}>Get a new meme image</button>
 
       <div className="main__image--container">
-        <img
-          src="http://i.imgflip.com/1bij.jpg"
-          alt="meme image"
-          className="main__image"
-        />
-        <span className="top">One does not simply</span>
-        <span className="bottom">Walk into Mordor</span>
+        <img src={meme.imageUrl} alt="meme image" className="main__image" />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
       </div>
     </main>
   );
